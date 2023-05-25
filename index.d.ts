5862a64a42caf53b140afaa2ee1537ee70119ed7
lee88688 <lee88688@163.com>
2023-05-25 14:31:29 +0000

    fix: type issue

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,31 @@
-declare module 'fast-mhtml' {
-  interface IParserPart {
-    location: string;
-    rewriteLocation?: string;
-    id?: string;
-    type: string;
-    encoding: string;
-    body: Uint8Array;
-  }
-
-  export interface IParserConfig {
-    rewriteFn?: (url: string, part: IParserPart) => string;
-    maxFileSize?: number
-  }
-  export interface IFileResult {
-    content: Uint8Array | string;
-    type: string;
-    filename: string;
-  }
-  export default class Parser {
-    constructor(config?: IParserConfig);
-    parse(contents: Uint8Array | string): this;
-    rewrite(): this;
-    spit(): IFileResult[];
-  }
-  // export class Converter {
-  //   static serve(port?: number): void;
-  //   static convert(filename: string): Promise<void>;
-  // }
+interface IParserPart {
+  location: string;
+  rewriteLocation?: string;
+  id?: string;
+  type: string;
+  encoding: string;
+  body: Uint8Array;
 }
+
+export interface IParserConfig {
+  rewriteFn?: (url: string, part: IParserPart) => string;
+  maxFileSize?: number
+}
+
+export interface IFileResult {
+  content: Uint8Array | string;
+  type: string;
+  filename: string;
+}
+
+export default class Parser {
+  constructor(config?: IParserConfig);
+  parse(contents: Uint8Array | string): this;
+  rewrite(): this;
+  spit(): IFileResult[];
+}
+
+// export class Converter {
+//   static serve(port?: number): void;
+//   static convert(filename: string): Promise<void>;
+// }
